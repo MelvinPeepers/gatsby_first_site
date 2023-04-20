7c7440c58b9427fa898a9d5472c1195749cd5cbb
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-04-20 07:07:49 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My_First_Gatsby_Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
