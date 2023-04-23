d3b5143a3561691351b6ea0ce445ac5c06b9eebd
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-04-23 07:40:50 -0400

    second commeit

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About Me</h1>
+            <p>Hi there! I'm a proud creator of this site, which I built with Gatsby.</p>
+        </main>
+    )
+}
+
+export const Head = () => (
+
+<>
+<title>About Me</title> 
+<meta name="description" content="My About Page" />
+</>
+)
+
+export default AboutPage
