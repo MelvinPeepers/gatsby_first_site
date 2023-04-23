382a88b91f104c95aee89a054eea69555ddf0a9d
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-04-23 11:22:46 -0400

    added image from images folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => {
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking tired at the camera"
-        src="https://gatsbytutorialexample.gatsbyjs.io/static/f7824272fbac2fda6a7bdb6f3567275f/5cc0d/clifford.webp"
+        src="../images/border-collie-1.jpg"
       />
     </Layout>
 
