6101ebff97630dbc569b470e66885d920380df2a
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-04-23 11:13:01 -0400

    added plugin image

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `My_First_Gatsby_Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
