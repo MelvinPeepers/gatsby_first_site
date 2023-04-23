ed440d82f371ee4a6bea37d007fd1a26e21c103f
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-04-23 10:20:00 -0400

    completed part 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 // Step 1: Import React
 import * as React from 'react'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to my Gatsby site!</h1>
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    </Layout>
+
+
   )
 }
 
@@ -15,8 +17,8 @@ const IndexPage = () => {
 export const Head = () => (
 
   <>
-  <title>Home Page</title> 
-  <meta name="description" content="My First Page" />
+    <title>Home Page</title> 
+    <meta name="description" content="My Home Page" />
   </>
   )
 
