e0f0158262b9a7d43d2fed09f822f08aa3407574
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-05-04 06:48:07 -0400

    wildcard redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,17 @@
 exports.createPages = async ({ graphql, actions }) => {
     const { createRedirect } = actions
   
+    // createRedirect({
+    //   fromPath: `/blog/my-sixth-post/`,
+    //   toPath: `/blog/my-fifth-post`,
+    // })
+    // the above redirects from /blog/my-sixth-post (which doesn't exist) to /blog/my-fifth-post which does exist 
+
+    // Wildcard path redirects
     createRedirect({
-      fromPath: `/blog/my-sixth-post/`,
-      toPath: `/blog/my-fifth-post`,
+      fromPath: `/blog/*`,
+      toPath: `/about`
     })
   }
 
-  // the above redirects from /blog/my-sixth-post (which doesn't exist) to /blog/my-fifth-post which does exist 
+  
