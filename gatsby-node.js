eb53176674fc5a8d69d0cfb3adc04470a6d6eeaa
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-05-07 11:42:42 -0400

    trying to setup dsg

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,6 +22,46 @@
 //     })    
 //   }
 
-  
+const path = require('path')
 
-  
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const result = await graphql(`
+    query {
+      allMdx(filter: {fileAbsolutePath: {regex: "/content/blog/"}}) {
+        edges {
+          node {
+            id
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    throw result.errors
+  }
+
+  // Create blog post pages.
+  const posts = result.data.allMdx.edges
+
+  // Create a page for each mdx file in the blog directory
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+
+    createPage({
+      path: `/${post.node.frontmatter.slug}`,
+      component: path.resolve(`./src/pages/blog-post.js`),
+      context: {
+        id: post.node.id,
+        previous,
+        next,
+      },
+    })
+  })
+}
--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -5,18 +5,19 @@ import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
 const BlogPost = ({ data, children }) => {
-    const image = getImage(data.mdx.frontmatter.hero_image)
+    const frontmatter = data.mdx.frontmatter || {};
+    const image = getImage(frontmatter.hero_image)
     return (
-        <Layout pageTitle={data.mdx.frontmatter.title}>
-            <p>{data.mdx.frontmatter.date}</p>
+        <Layout pageTitle={frontmatter.title}>
+            <p>{frontmatter.date}</p>
             <GatsbyImage
                 image={image}
-                alt={data.mdx.frontmatter.hero_image_alt}
+                alt={frontmatter.hero_image_alt}
             />
             <p>
                 Photo Credit:{" "}
-                <a href={data.mdx.frontmatter.hero_image_credit_link}>
-                    {data.mdx.frontmatter.hero_image_credit_text}
+                <a href={frontmatter.hero_image_credit_link}>
+                    {frontmatter.hero_image_credit_text}
                 </a>
             </p>
             {children}
