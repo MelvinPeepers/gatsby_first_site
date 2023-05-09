fc311f367f99485b41faa6bb54c768071049e780
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-05-09 06:37:26 -0400

    re-added redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,25 +1,20 @@
-// exports.createPages = async ({ graphql, actions }) => {
-//     const { createRedirect } = actions
+exports.createPages = async ({ graphql, actions }) => {
+    const { createRedirect } = actions
   
-//     // createRedirect({
-//     //   fromPath: `/blog/my-sixth-post/`,
-//     //   toPath: `/blog/my-fifth-post`,
-//     // })
-//     // the above redirects from /blog/my-sixth-post (which doesn't exist) to /blog/my-fifth-post which does exist 
+    // createRedirect({
+    //   fromPath: `/blog/my-sixth-post/`,
+    //   toPath: `/blog/my-fifth-post`,
+    // })
+    // the above redirects from /blog/my-sixth-post (which doesn't exist) to /blog/my-fifth-post which does exist 
 
-//     // Wildcard path redirects
-//     createRedirect({
-//       fromPath: `/blog/*`,
-//       toPath: `/blog`
-//     })
-//         // the above wildcard redirect rule redirects back to the blog page if the page doesn't exist 
-
-//     createPage({
-//       path: "page-path",
-//       component: "component-path",
-//       context: {},
-//       defer: true,
-//     })    
-//   }
+    // Wildcard path redirects
+    createRedirect({
+      // fromPath: Any valid URL. Must start with a forward slash  
+      fromPath: `/blog/*`,
+      // toPath: URL of a created page (see createPage)
+      toPath: `/blog`
+    })
+        // the above wildcard redirect rule redirects back to the blog page if the page doesn't exist 
+  }
 
 
