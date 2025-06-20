964ce19ed367abcf98de24acdf9ea7f50a9a9d75
Melvin Peepers <melvinpeepers@Mac.lan>
2025-06-20 11:37:36 -0500

    added some console statements to the ssr.js page

--- src/pages/ssr.js
+++ src/pages/ssr.js
@@ -11,17 +11,28 @@ const SSRPage = ({ serverData }) => (
 export default SSRPage
 
 export async function getServerData() {
+    console.time("getServerData total");
+    console.log("Memory before fetch:", process.memoryUsage());
     try {
+        console.time("fetch dog image");
         const res = await fetch(`https://dog.ceo/api/breeds/image/random`)
+        console.timeEnd("fetch dog image");
 
         if (!res.ok) {
             throw new Error(`Response failed`)
         }
 
+        const json = await res.json();
+
+        console.log("Memory after fetch", process.memoryUsage());
+        console.timeEnd("getServerData total");
+
         return {
             props: await res.json(),
         }
     } catch (error) {
+        console.error("Error in getServerData:", error);
+        console.timeEnd('getServerData total');
         return {
             status: 500,
             headers: {},
